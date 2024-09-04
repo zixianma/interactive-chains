61336d647dcf1b03542921f89f954db2b9d53c75
Ziang Li <ziangl4@cs.washington.edu>
2024-09-04 01:34:15 -0700

    debug hotjar

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ def main():
     
     components.html(
         '''
-        <script type="text/javascript">
-            console.log('Clarity script is loading...');
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "nxtbql5obv");
-            console.log('Clarity script is loaded');
+        <!-- Hotjar Tracking Code for streamlit app -->
+        <script>
+            console.log('Hotjar script is loading...');
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5120950,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            hj('debug', true);  // Enable Hotjar debug mode
+            console.log('Hotjar script initialized');
         </script>
         ''',
         height=0,
