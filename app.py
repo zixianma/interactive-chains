b5fc8446484f406a2c241adf805f76be3790c29d
Ziang Li <ziangl4@cs.washington.edu>
2024-09-05 03:04:42 -0700

    debug hotjar

--- app.py
+++ app.py
@@ -9,27 +9,6 @@ from pages.instruction import instruction
 import streamlit.components.v1 as components
 
 def main():
-    
-    components.html(
-        '''
-        <!-- Hotjar Tracking Code for streamlit app -->
-        <script>
-            console.log('Hotjar script is loading...');
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:5120950,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            hj('debug', true);  // Enable Hotjar debug mode
-            console.log('Hotjar script initialized');
-        </script>
-        ''',
-        height=0,
-        width=0
-    )
 
     st.markdown(
         """
