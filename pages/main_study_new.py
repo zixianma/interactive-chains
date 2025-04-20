5df1ca51c0359bba40101dc3fb57f214e829c75e
Ziang Li <ziangl4@cs.washington.edu>
2025-04-20 14:42:44 -0700

    style fixing

--- pages/main_study_new.py
+++ pages/main_study_new.py
@@ -419,7 +419,7 @@ def main_study():
             stage_key="study_transition_done",
             stage_title="The Study Phase",
             instructions=(
-                "You have now entered the Study Phase. In this phase, you will answer 12 questions. "
+                "You have now entered the Study Phase. In this phase, you will answer 12 questions.\n\n "
                 "Your performance will determine your reward, and you will NOT be shown whether your answer is correct.\n\n"
                 ":red[**You will be provided AI assistance throughout the study. Please do not use any other external tools or AI models during the study. "
                 "We might reject your submission if we believe your answers are abnormal due to the use of other tools.**]\n\n"
