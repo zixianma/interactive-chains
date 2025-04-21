9b790442da7272901eaed045e2255c74f51ddf81
Ziang Li <ziangl4@cs.washington.edu>
2025-04-21 09:19:25 -0700

    Change condition to paragraph only

--- pages/login.py
+++ pages/login.py
@@ -168,8 +168,8 @@ def submit_consent(username_input):
                 condition_counts = get_condition_counts(pilot_worksheet)
                 # this is to make static chain 2x as likely
                 weights = {
-                    "A. Answer only" : 1,
-                    "B. Paragraph CoT" : 0.0,
+                    "A. Answer only" : 0.0,
+                    "B. Paragraph CoT" : 1.0,
                     "C. Step-by-step CoT -- All at once": 0.0,
                     "D. Step-by-step CoT -- Sequential": 0.0,
                     "E. Verifiable CoT": 0.0
